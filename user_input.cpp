105dd43dc8e5432cfdb2852c75c7b318d08dd1d8
Dmitry Dmitriev <ddmitriev@freedommobile.ca>
2019-07-04 12:56:45 -0400

    Play with cin

---
+++ user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string userInput;
+    cin >> userInput;
+    cout << "User input: " + userInput + "\n";
+    return 0;
+}
