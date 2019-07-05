d2baa4871f16c825ad9e88c6584938dd46c9cda1
Dmitry Dmitriev <ddmitriev@freedommobile.ca>
2019-07-05 14:04:45 -0400

    Add function example

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sum (int a, int b) {
+    return a + b;
+}
+
+int main()
+{
+    int s;
+    s = sum(10, 5);
+    cout << s << endl;
+    return 0;
+}
